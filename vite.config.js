3c4848c0ac486a1eaa9db64c1fda680ea84b07a5
KUMAR <kumar@KUMARs-MacBook-Air.local>
2025-09-23 00:38:37 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-    base: '/reactdeployex2/',
+    base: '/reactdeployex2/', // Repo name matches your GitHub repo name!
     plugins: [react()],
 })
